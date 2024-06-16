4cdb6c38bc2d34acb547fc9f981dd2c3a8227d6c
VolkKatasi <makarovdmitriy2005@gmail.com>
2024-06-16 11:51:17 +0400

    Переходы

--- Form1.cs
+++ Form1.cs
@@ -42,6 +42,7 @@ namespace WindowsFormsApp1
 
         private void label1_Click(object sender, EventArgs e)
         {
+            this.Hide();
             Bron bron = new Bron();
             bron.Show();
         }
