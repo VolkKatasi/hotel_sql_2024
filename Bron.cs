6733892e4993c32ec65cca107c1ef61bc8d54e4c
Tim4ukys <tim4ukys.dev@yandex.ru>
2024-06-17 18:35:17 +0700

    fix

--- Bron.cs
+++ Bron.cs
@@ -55,6 +55,8 @@ namespace WindowsFormsApp1
         private void label5_Click(object sender, EventArgs e)
         {
             Form4 form4 = new Form4();
+            this.Hide();
+            form4.FormClosed += (Object, FormClosedEventArgs) => { langInit(); this.Show(); };
             form4.Show();
         }
     }
